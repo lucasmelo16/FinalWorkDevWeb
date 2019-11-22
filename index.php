ba96eecea0ab0d7e8dc431822a62526f8752df16
Lucas Patrocínio Melo - 11816233 <11816233@animaedu.intranet>
2019-11-22 20:38:58 -0300

    teste

---
+++ index.php
@@ -0,0 +1,16 @@
+<<?php  
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TV GAMES</title>
+</head>
+<body>
+	<center>
+	<h1>O maior site de trocas e vendas de jogos do Brasil</h1>
+	</center>
+	
+
+</body>
+</html>
